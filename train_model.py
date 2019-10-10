e2a7dcdbe9f185629031482b5535a55a68d67bb1
Nate Sampo <nathan.sampo@students.olin.edu>
2019-10-10 10:52:56 -0400

    progress 10/10

--- train_model.py
+++ train_model.py
@@ -1,23 +1,41 @@
 from PIL import Image
 import numpy as np
-import pytorch
+import torch
+#import torchvision
+#import torchvision.transforms as transforms
 import pandas
+import os
 
 color_dir = 'color_imgs'
 grayscale_dir = 'grayscale_imgs'
 
-def get_images(path):
-    paths = os.listdir(path)
+def get_images(input_dir):
+    paths = os.listdir(input_dir)
     img_paths = [item for item in paths if item.lower().endswith(".jpg")]
     for path in img_paths:
         if not path.endswith('_0.jpg') and not path.endswith('_5.jpg'):
             image = Image.open(os.path.join(input_dir, path))
             yield image
 
-if __name__ == '__main__':
-	data = np.array([])
-	imgs = get_images(color_dir)
-	for i in imgs:
-		np.concatenate([data, np.asarray(i)], axis = 2)
+def format_color_images():
+    imgs = list(get_images(color_dir))[:4]
+    data = [np.expand_dims(np.array(i), axis=3) for i in imgs]
+    data = np.concatenate((*data,), axis=3)
+    return data
 
-	print(data)
+def format_grayscale_images():
+    imgs = list(get_images(grayscale_dir))[:4]
+    data = [np.expand_dims(np.array(i), axis=3) for i in imgs]
+    data = np.concatenate((*data,), axis=2)
+    return data
+
+class ColorizeCNN(torch.nn.Module):
+    def __init__(self):
+        super(ColorizeCNN, self).__init__()
+
+        
+
+if __name__ == '__main__':
+    data = format_grayscale_images()
+
+    print(data.shape)
